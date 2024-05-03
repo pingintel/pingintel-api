17d78140f9d554132baaef2796086a803397143d
Brett Buford <blbuford@gmail.com>
2024-05-03 14:43:34 -0700

    Status Enum Exports

--- src/pingintel_api/__init__.py
+++ src/pingintel_api/__init__.py
@@ -1,2 +1,3 @@
 from .pingvision.pingvision_api_client import PingVisionAPIClient
 from .sov_fixer.sov_fixer_api_client import SOVFixerAPIClient
+from .sov_fixer.types import INCOMPLETE_STATUSES, SOV_RESULT_STATUS, SOV_STATUS
