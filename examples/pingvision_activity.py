2901bf055c9da6816660d48b4c242616c59b1c08
Joseph Misiti <joseph.misiti@mathandpencil.com>
2024-09-02 15:17:56 -0400

    Add functionality to create submissions for multiple SOV files using PingVisionAPIClient.

--- examples/pingvision_activity.py
+++ examples/pingvision_activity.py
@@ -7,12 +7,28 @@ site.addsitedir("../src")
 from pingintel_api import PingVisionAPIClient
 
 SCRIPT_DIR = pathlib.Path(__file__).parent
+print(SCRIPT_DIR)
 
-api_client = PingVisionAPIClient(environment="dev")
+api_client = PingVisionAPIClient(environment="local")
 
-ret = api_client.create_submission(
-    files={"files": ("test_sov.xlsx", open(SCRIPT_DIR / "test_sov.xlsx", "rb"))}
-)
+TEST_DATA_SOV_DIR = "/Users/josephmisiti/mathandpencil/projects/boxandwhisker/boxandwhisker-sov-scrubber/testdata/SOV"
+
+SOVS = [
+    "8696955-2019.Statement.of.Values_ProspectPlace_123119-20_1.XLSX",
+    "AAA Test SOV.XLSX",
+    "Delaney at Parkway SOV (8-9-18).xls",
+    "Marketing xls.xlsx",
+    "mario (1).xlsx",
+    "Scot Holding Assurant Flood Chart-VOYAGER-WB.xlsx"
+]
+
+for sov in SOVS:
+    ret = api_client.create_submission(
+        files={"files": (sov, open(TEST_DATA_SOV_DIR + "/" + sov, "rb"))}
+    )
+
+
+
 print(f"pingid: {ret['id']}")
 activity_results = api_client.list_submission_activity(page_size=5)
 
