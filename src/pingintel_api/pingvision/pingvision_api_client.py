7b31eb9f4ce1e59c30e5a2062c2ffc0ab6da6423
Sy Khader <smskhader@gmail.com>
2026-01-29 13:22:52 -0500

    Add support for do_not_certify API param

--- src/pingintel_api/pingvision/pingvision_api_client.py
+++ src/pingintel_api/pingvision/pingvision_api_client.py
@@ -39,6 +39,7 @@ class PingVisionAPIClient(APIClientBase):
         expiration_date: datetime.date | None = None,
         delegate_to_company: str | None = None,
         delegate_to_team: str | None = None,
+        do_not_certify: bool = False,
     ) -> t.PingVisionCreateSubmissionResponse:
         """
         Initiate a new submission from one or more original files.
@@ -92,6 +93,8 @@ class PingVisionAPIClient(APIClientBase):
             data["delegate_to_company"] = delegate_to_company
         if delegate_to_team:
             data["delegate_to_team"] = delegate_to_team
+        
+        data["do_not_certify"] = do_not_certify
 
         response = self.post(url, files=multiple_files, data=data)
 
