078633e6ecfa0fb9e93ca00fd1b5ab39c8250bee
andrew misiti <andrew.misiti@mathandpencil.com>
2026-07-30 15:37:40 -0400

    ct-308 updating type responses for jobs

--- src/pingintel_api/pingvision/types.py
+++ src/pingintel_api/pingvision/types.py
@@ -32,7 +32,7 @@ class PingVisionListActivityDetailJobSovFixerDetailResponse(TypedDict):
     sovfixer_result_message: NotRequired[str | None]
     sovfixer_request_type: NotRequired[str | None]
     job_type_label: NotRequired[str | None]
-    cat_model_types: NotRequired[list[Literal['RMS', 'AIR']]] | None
+    cat_model_types: NotRequired[list[Literal['RMS', 'AIR']] | None]
 
 
 class DOCUMENT_PROCESSING_STATUS(str, enum.Enum):
